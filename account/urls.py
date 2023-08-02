1672c71af253091dfda85cfc98fc50cbb38050ab
Aziza <azizamukashkyzy@gmail.com>
2023-08-03 01:44:24 +0600

    Added gitignore

--- account/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import CreateUserView, CreateTokenView, ManageUserView
-
-app_name = 'user'
-
-urlpatterns = [path('register/', CreateUserView.as_view(), name='register'),
-               path('token/', CreateTokenView.as_view(), name='token'),
-               path('user-detail/', ManageUserView.as_view(), name='user-detail')]
