308521b1ec543188844c82f6b6587bdd4da6a361
Aziza <azizamukashkyzy@gmail.com>
2023-08-21 14:19:28 +0600

    Product filed image modified

--- product/models.py
+++ product/models.py
@@ -18,7 +18,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
-    url = models.ImageField(null=True, blank=True, verbose_name="Изображение")
+    url = models.URLField(null=True, blank=True, verbose_name="Изображение")
     name = models.CharField(max_length=50, verbose_name="Название")
     desc = models.CharField(max_length=255, verbose_name="Описание")
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0,
